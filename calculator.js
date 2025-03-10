06cdac84ff76bf2f1ddf7d3952ca41a46dc3ef92
badhusha <muhammadbadhusha7@gmail.com>
2025-03-10 09:34:17 +0530

    initail commit

---
+++ calculator.js
@@ -0,0 +1,10 @@
+function getValue(value){
+document.getElementById('display').value+=value
+
+}
+function  clearDisplay(){
+    document.getElementById('display').value=""
+}
+function calculate(){
+    document.getElementById('display').value= eval(document.getElementById('display').value)
+}
